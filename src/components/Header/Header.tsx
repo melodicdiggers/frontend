70800ac703f6df147dd60c0e62ad19b75996b23c
Joao Alves <jmca-2000@hotmail.com>
2023-04-27 21:27:49 +0100

    feat: getHeaderData

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,30 @@ import Menu from './Menu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
+async function getHeaderData(): Promise<IHeader | null> {
+	try {
+		const result = await getHeader()
+		if (result) {
+			return result.header.data.attributes
+		}
+		return null
+	} catch (err) {
+		return null
+	}
+}
+
 export default async function Header() {
-	const [result] = await Promise.all([getHeader()])
-	const header: IHeader = result.header.data.attributes
+	const header: IHeader | null = await getHeaderData()
+	console.log(header)
 
 	return (
 		<nav className='w-full px-44 py-10 sm:px-2 sm:py-6 md:px-6 lg:px-4'>
 			<div className='flex items-center justify-between'>
 				<Link href={'/'} className='items-center font-josefin text-3xl font-semibold text-black no-underline'>
-					{header.title.toUpperCase()}
+					{header?.title.toUpperCase()}
 				</Link>
 				<div className='flex flex-row gap-12 sm:hidden'>
-					{header.headerBlock.map((block: IHeaderBlock) => {
+					{header?.headerBlock.map((block: IHeaderBlock) => {
 						return <Menu block={block} key={`header-block-${block.title}`} />
 					})}
 				</div>
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,13 +1,14 @@
 import { DocumentNode } from 'graphql'
 import { graphqlClient } from '../services/client'
 import { HeaderDocument, LandingPhotoDocument, Sdk } from '../generated/sdk'
+import { Variables } from 'graphql-request'
 
 export type QueryName = keyof Sdk
 
 export const fetchGraphQLData = async <Q, V>(query: DocumentNode, variables?: V) => {
 	try {
 		let response = null
-		response = await graphqlClient.request<Q>(query, variables)
+		response = await graphqlClient.request<Q>(query, variables as Variables)
 		return response
 	} catch (error) {
 		return null
