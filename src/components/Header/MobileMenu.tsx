c7efbce192935e1d36f3218a823863566eaa2dd7
Joao Alves <jmca-2000@hotmail.com>
2023-05-11 15:47:52 +0100

    feat: mobile header completed

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Link from 'next/link'
 import { IHeaderBlock, IOptions } from '../../types'
 import { useState } from 'react'
@@ -19,31 +18,31 @@ export default function MobileMenu({ block, setMobileNavbar }: MenuProps) {
 		setMobileNavbar(false)
 	}
 
-	console.log(block)
 	return (
 		<div>
 			{block.title === 'News' ? (
 				<Link className='cursor-pointer text-black no-underline' href={'/news'} onClick={() => closeMobile()}>
-					<div className='text-xl font-semibold'>{block.title.toUpperCase()}</div>
+					<div className='font-josefin text-xl font-semibold'>{block.title.toUpperCase()}</div>
 				</Link>
 			) : (
-				<div className='text-xl font-semibold' onClick={toggleOptions}>
+				<div className=' font-josefin text-xl font-semibold' onClick={toggleOptions}>
 					{block.title.toUpperCase()}
-				</div>
-			)}
-			{showOptions && block.title !== 'News' && (
-				<div className=' bg-background px-2 pb-0 pt-4'>
-					{block.options.map((option: IOptions) => {
-						return (
-							<Link
-								href={`${option.route}`}
-								key={`option-${option.slug}`}
-								onClick={() => closeMobile()}
-								className='flex-column mb-2 flex cursor-pointer text-base font-semibold text-black no-underline'>
-								{option.title.toUpperCase()}
-							</Link>
-						)
-					})}
+					<div
+						className={`max-h-0 w-full overflow-hidden bg-background transition-all duration-700 ease-in-out ${
+							showOptions ? 'max-h-screen' : ''
+						}`}>
+						{block.options.map((option: IOptions) => {
+							return (
+								<Link
+									href={`${option.route}`}
+									key={`option-${option.slug}`}
+									onClick={() => closeMobile()}
+									className='flex-column hover:bg-gray-200 mb-2 mt-2 flex cursor-pointer px-4 font-josefin text-base font-semibold text-black no-underline'>
+									{option.title.toUpperCase()}
+								</Link>
+							)
+						})}
+					</div>
 				</div>
 			)}
 		</div>
