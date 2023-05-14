a62d91bc3e21c369d8828abff8c891fa606910af
Joao Alves <jmca-2000@hotmail.com>
2023-05-14 18:40:42 +0100

    feat: image carrousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import DefaultImage from '../../assets/images/DefaultImage.jpg'
 import MediaGallery from '../../components/LandingPage/MediaGallery'
+
 export default async function Homepage() {
 	return (
 		<div className='flex flex-col'>
@@ -11,9 +12,11 @@ export default async function Homepage() {
 				width={2000}
 				height={560}
 				quality={100}
+				priority
 				style={{ objectFit: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
 			/>
 			<div>Landing Page</div>
+			{/* @ts-expect-error Server Component */}
 			<MediaGallery />
 		</div>
 	)
--- src/components/LandingPage/MediaGallery.tsx
+++ src/components/LandingPage/MediaGallery.tsx
@@ -1,6 +1,6 @@
-'use client'
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import React from 'react'
 
 interface Photo {
 	id: string
@@ -8,29 +8,36 @@ interface Photo {
 	media_url: string
 }
 
-export default function MediaGallery() {
-	const [images, setImages] = useState<Photo[]>([])
+async function getInstagramImages(): Promise<Photo[]> {
+	try {
+		const response = await fetch(
+			`https://graph.instagram.com/me/media?fields=id,media_type,media_url&limit=10&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`,
+			{ cache: 'force-cache' }
+		)
+		const data = await response.json()
+		return data.data
+	} catch (err) {
+		return []
+	}
+}
 
-	useEffect(() => {
-		// Fetch the first 10 images from the Instagram account
-		const fetchImages = async () => {
-			const response = await fetch(
-				`https://graph.instagram.com/me/media?fields=id,media_type,media_url&limit=10&access_token=${'IGQVJYS2FhdEJuQktVNVctVUlVaG16dUdxcnR4WVFRbXNGd2FDOGxBeEtEaHVFOFZAuNzcyenB3WktPRFEyRWZArLWFIS2pEZA09hRnJ0WVdLMnB0YWhSaGpNQWM3Unp0cU44YUlXWXJsRUVOTEpUQ1p5MgZDZD'}`,
-				{ cache: 'force-cache' }
-			)
-			const data = await response.json()
-			setImages(data.data)
-		}
-
-		fetchImages()
-	}, [])
+export default async function MediaGallery() {
+	const images = await getInstagramImages()
 
 	return (
 		<div className='flex flex-col'>
-			<div className='grid grid-cols-5 gap-4 px-44'>
+			<div className='flex flex-wrap gap-4 px-44'>
 				{images.map(image => (
 					<div key={image.id}>
-						<Image src={image.media_url} alt='' width={250} height={250} />
+						<Link href={image.media_url}>
+							<Image
+								src={image.media_url}
+								alt=''
+								width={250}
+								height={250}
+								style={{ objectFit: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
+							/>
+						</Link>
 					</div>
 				))}
 			</div>
